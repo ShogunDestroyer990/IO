5fed837d56c8b239ac6cd25b3604bab42f43ad95
ShogunDestroyer990 <adam.godzinski@student.put.poznan.pl>
2017-11-14 22:28:56 +0100

    Add files via upload

---
+++ Zad 2-3-4.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+        
+
+            ThreadPool.QueueUserWorkItem(Server);
+
+            Thread.Sleep(1000);
+
+            ThreadPool.QueueUserWorkItem(Client);
+
+            Thread.Sleep(500);
+
+            ThreadPool.QueueUserWorkItem(Client);
+
+            Thread.Sleep(500);
+
+
+
+        }
+
+        static void WC(byte[] message, ConsoleColor color)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.ForegroundColor = color;
+                Console.Write(Convert.ToChar(message[i]));
+                Console.ResetColor();
+
+            }
+            Console.WriteLine();
+        }
+
+        static void Client(Object info)
+        {
+            Object thisLock = new Object();
+            Thread.Sleep(100);
+
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048));
+            var message = new ASCIIEncoding().GetBytes("text");
+            
+                client.GetStream().Write(message, 0, message.Length);
+            // Thread.Sleep(300);
+            lock (thisLock)
+            {
+                WC(message, ConsoleColor.Green);
+            }
+
+            
+
+
+        }
+
+        static void Server(Object info)
+        {
+            Thread.Sleep(100);
+            TcpListener server = new TcpListener(IPAddress.Any, 2048);
+            ThreadPool.QueueUserWorkItem(Client);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+               // ThreadPool.QueueUserWorkItem(Client);
+
+
+                var buffer = new byte[1024];
+                client.GetStream().Read(buffer, 0, 1024);
+                client.GetStream().Write(buffer, 0, buffer.Length);
+                Thread.Sleep(100);
+                WC(buffer, ConsoleColor.Red);
+                client.Close();
+
+            }
+        }
+
+
+
+    }
+
+
+}
---
+++ Zad5.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+
+
+
+    public class TaskInfo
+    {
+        
+       
+        public int Value;
+        public AutoResetEvent arvent;
+
+        public TaskInfo( int Value, AutoResetEvent arvent)
+        {
+           
+           this.Value = Value;
+            this.arvent = arvent;
+        }
+    }
+
+
+
+
+
+    class Program
+    {
+
+       public static int N = 10;
+
+        public static int suma;
+
+        static int[] tab = new int[] { 3, 4, 5, 6, 1, 4, 9, 12, 1, 7 };
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Object thisLock = new Object();
+
+
+            AutoResetEvent[] arvents = new AutoResetEvent[N];
+
+            //  Thread[] Tablica_wątków = new Thread[N];
+            for (int i = 0; i < N; i++)
+            {
+               
+                Random rnd = new Random();
+
+                int chosen = rnd.Next(0, N);
+
+
+                arvents[i] = new AutoResetEvent(false);
+
+                TaskInfo ti = new TaskInfo(tab[chosen], arvents[i]);
+
+
+               
+
+                ThreadPool.QueueUserWorkItem(new WaitCallback(wybierz_losowyelemnt),ti);
+                //  Tablica_wątków[i] = new Thread(wybierz_losowyelemnt);
+                //Tablica_wątków[i].Start();
+                Thread.Sleep(100);
+            }
+
+            WaitHandle.WaitAll(arvents);
+
+
+            lock (thisLock)
+            {
+                
+                System.Console.WriteLine(" wynik to " + suma);
+            }
+         
+
+
+        }
+
+
+
+        static void wybierz_losowyelemnt(object info)
+        {
+
+            TaskInfo ti = (TaskInfo)info;
+
+            System.Console.WriteLine(ti.Value);
+
+            suma += ti.Value;
+
+
+            ti.arvent.Set();
+
+
+        }
+    }
+}
+
+
+/*
+ 
+   bool notchosen = true;
+            int[] itwas=new int [N];
+            int size = itwas.Length;
+
+
+            while (notchosen)
+            {
+                
+
+                for (int i = 0; i < size; i++)
+                {
+
+                    if (itwas[i] == chosen)
+                    {
+                        notchosen = false;
+                    }
+                    
+                }
+
+                if (notchosen == false)
+                {
+                    notchosen = true;
+                }
+                else
+                {
+                    notchosen = false;
+                    itwas[size - 1] = chosen;
+                    System.Console.WriteLine(chosen);
+                }
+
+                 
+            }
+ 
+
+    */
